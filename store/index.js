a59b706af10dae91f09e0b72bc53672968fea004
kangfa <1470298128@qq.com>
2021-04-28 11:10:03 +0800

    准备设计store

---
+++ store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	state:{
+		types: [],
+		intervals:[],
+		authorzation: ''
+	},
+	mutations:{
+		
+	},
+	actions:{
+		downloadType(){
+			
+		}
+	}
+	
+})
+
+export default store;
